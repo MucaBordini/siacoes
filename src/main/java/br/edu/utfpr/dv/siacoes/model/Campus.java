4168d2da2c2af4cd200d3f9652402824288f5c4f
MucaBordini <mucabordini@gmail.com>
2020-08-26 12:56:42 -0300

    Alterações na classe Campus do package Model
    Foram acrescentadas anotações da biblioteca Lombok para diminuir o código boilerplate.
    Anotações usadas:
    @ToString(onlyExplicitlyIncluded = true) para gerar um ToString apenas para as variáveis marcadas com o Include.
    @NoArgsConstructor para gerar o construtor da classe sem passar parâmetros.
    @Getter @Setter para gerar os getters e setters das variáveis, exceto a variável "active" pois seu método get é na verdade "isActive" e alterar isso comprometeria outras classes que chamem esse método.
    @ToString.Include usado para incluir o campo "name" no método ToString.

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Campus.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Campus.java
@@ -2,73 +2,27 @@
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
 public class Campus implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private int idCampus;
-	private String name;
-	private String address;
-	private transient byte[] logo;
-	private boolean active;
-	private String site;
-	private String initials;
+	private @Getter @Setter int idCampus;
+	@ToString.Include
+	private @Getter @Setter String name;
+	private @Getter @Setter String address;
+	private @Getter @Setter transient byte[] logo;
+	private @Setter boolean active;
+	private @Getter @Setter String site;
+	private @Getter @Setter String initials;
 	
-	public Campus(){
-		this.setIdCampus(0);
-		this.setName("");
-		this.setAddress("");
-		this.setLogo(null);
-		this.setActive(true);
-		this.setSite("");
-		this.setInitials("");
-	}
-	
-	public int getIdCampus() {
-		return idCampus;
-	}
-	public void setIdCampus(int idCampus) {
-		this.idCampus = idCampus;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public byte[] getLogo() {
-		return logo;
-	}
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
 	public boolean isActive() {
 		return active;
 	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getSite(){
-		return site;
-	}
-	public void setSite(String site){
-		this.site = site;
-	}
-	public String getInitials() {
-		return initials;
-	}
-	public void setInitials(String initials) {
-		this.initials = initials;
-	}
-
-	public String toString(){
-		return this.getName();
-	}
-
 }
