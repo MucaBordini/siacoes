4fa68c0948f0c51ec08d42ff3377e61538db3bf3
MucaBordini <mucabordini@gmail.com>
2020-08-26 12:44:15 -0300

    Alterações no Department do package Model
    Foram adicionadas as anotações Lombok @Getter e @Setter nas váriaveis que utlizavam getter e setter, exceto na "active" pois o seu get era na verdade isActive, portanto foi mantido assim. Foi adicionado a anotação @ToString.Include para mostrar qual variável teria que gerar um ToString, neste caso foi a "nome". E por fim a anotação @NoArgsConstructor que gera o construtor da classe que não recebe parâmetros.

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
@@ -2,81 +2,29 @@
 
 import java.io.Serializable;
 
+import lombok.ToString;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@ToString(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
 public class Department implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private int idDepartment;
-	private Campus campus;
-	private String name;
-	private String fullName;
-	private transient byte[] logo;
-	private boolean active;
-	private String site;
-	private String initials;
+	private @Getter @Setter int idDepartment;
+	private @Getter @Setter Campus campus;
+	@ToString.Include
+	private @Getter @Setter String name;
+	private @Getter @Setter String fullName;
+	private @Getter @Setter transient byte[] logo;
+	private @Setter boolean active;
+	private @Getter @Setter String site;
+	private @Getter @Setter String initials;
 	
-	public Department(){
-		this.setIdDepartment(0);
-		this.setCampus(new Campus());
-		this.setName("");
-		this.setFullName("");
-		this.setLogo(null);
-		this.setActive(true);
-		this.setSite("");
-		this.setInitials("");
-	}
-	
-	public int getIdDepartment() {
-		return idDepartment;
-	}
-	public void setIdDepartment(int idDepartment) {
-		this.idDepartment = idDepartment;
-	}
-	public Campus getCampus() {
-		return campus;
-	}
-	public void setCampus(Campus campus) {
-		this.campus = campus;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getFullName(){
-		return fullName;
-	}
-	public void setFullName(String fullName){
-		this.fullName = fullName;
-	}
-	public byte[] getLogo() {
-		return logo;
-	}
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
 	public boolean isActive() {
 		return active;
-	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getSite(){
-		return site;
-	}
-	public void setSite(String site){
-		this.site = site;
-	}
-	public String getInitials() {
-		return initials;
-	}
-	public void setInitials(String initials) {
-		this.initials = initials;
-	}
-	
-	public String toString(){
-		return this.getName();
 	}
 	
 }
