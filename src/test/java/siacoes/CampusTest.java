e5e6467ed8f3f09fba3401327a312dfcc7f4315e
MucaBordini <mucabordini@gmail.com>
2020-09-10 14:33:13 -0300

    Adicionados dois casos de teste
    Utilizando a tecnologia JUnit foram implementados dois casos de teste, um para a classe Campus e outro para a classe Department.
    O primeiro testa se os métodos setSite() e getSite() estão funcionando corretamente após a adição da biblioteca Lombok.
    O segundo testa se os métodos setFullName() e getFullName() estão funcionando corretamente após a adição da biblioteca Lombok.

---
+++ src/test/java/siacoes/CampusTest.java
@@ -0,0 +1,27 @@
+package siacoes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.edu.utfpr.dv.siacoes.model.Campus;
+
+class CampusTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		Campus campus = new Campus();
+		campus.setSite("www.testandocampus.com");
+		
+		String resultadoEsperado = "www.testandocampus.com";
+		String resultadoObtido = campus.getSite();
+		
+		assertEquals(resultadoEsperado, resultadoObtido, "PASSOU!");
+	}
+
+}
--- src/test/java/siacoes/DepartmentTest.java
+++ src/test/java/siacoes/DepartmentTest.java
@@ -2,13 +2,22 @@ package siacoes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import br.edu.utfpr.dv.siacoes.model.Department;
+
 import org.junit.jupiter.api.Test;
 
 class DepartmentTest {
-
+	
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Department dep = new Department();
+		dep.setFullName("Testando Set e Get FullName");
+		
+		String resultadoEsperado = "Testando Set e Get FullName";
+		String resultadoObtido = dep.getFullName();
+		
+		assertEquals(resultadoEsperado, resultadoObtido);
 	}
 
 }
